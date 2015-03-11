3946d8428a2bfe5d319d0989344893da5ab8384f
Shohan Hasan <shohan.hasan@nyu.edu>
2015-03-10 20:33:56 -0400

    added bare bone template for new scripts

---
+++ Template.rb
@@ -0,0 +1,17 @@
+# Purpose
+
+# Author
+# Affiliation and/or e-mail
+# Date
+
+require 'Datavyu_API.rb'
+
+begin
+	# Parameters.
+	verbose = 1		# control how much to output to console
+
+
+rescue StandardError => e
+	puts e.message
+	puts e.backtrace
+end
